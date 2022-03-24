e67153e92a08f3689b144115991c4a1ad973125c
Your Name <keefevuong123@gmail.com>
2022-03-24 18:29:20 +1100

    Testing out the waters

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = ({title}) => {
+  return (
+    <header class='header'>
+        <h1>{title}</h1>
+        <button classname='button'>Click To Play</button>
+    </header>
+  )
+}
+
+export default Header
